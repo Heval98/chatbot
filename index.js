b4db31ffa747806e660b6b27f25ce87dbc2dbe46
Heval98 <jmheval@gmail.com>
2025-05-11 18:12:29 -0500

    vercel config

--- index.js
+++ index.js
@@ -54,6 +54,5 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
-});
+// Export the app for Vercel
+module.exports = app;
